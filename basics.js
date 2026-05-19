bea0500a64ff0306aa15c6441892e609890159d7
Elijah736 <elijahlategan3@gmail.com>
2026-05-19 14:47:17 +0200

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,7 @@
+const firstName = "Elijah";
+const lastName = "Lategan";
+let age = 19;
+let fullName = `${firstName} ${lastName}`;
+console.log(`Hello ${fullName}, you are ${age} years old!`);
+age = 25
+console.log(`Hello ${fullName}, you are now ${age} years old!`);
