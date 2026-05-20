b447d276896073a17e845348d931d7015bdb8b18
Elijah736 <elijahlategan3@gmail.com>
2026-05-20 15:06:57 +0200

    Update operations.js

---
+++ operations.js
@@ -0,0 +1,13 @@
+let number1 = 10;
+let number2 = 15;
+let num = number1.toString();
+let sum = number1 + number2;
+let product = number1 * number2;
+console.log(number1)
+console.log(number2)
+console.log(`${num} as a string `)
+console.log(sum)
+console.log(product)
+// Checks if num is a string
+console.log(typeof(num))
+
