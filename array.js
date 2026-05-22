985c04b5b1e37f356a24e53a7f7d09defbfa0b3f
Elijah736 <elijahlategan3@gmail.com>
2026-05-22 14:55:32 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+let colors = [];
+
+colors.push("blue");
+colors.push("green");
+colors.push("red");
+
+console.log(colors);
+
+let numbers = [1, 9, 3, 6, 7];
+
+colors.pop();
+
+colors.unshift("red");
+
+console.log(colors.length);
+console.log(numbers.length);
+console.log(colors);
+console.log(numbers);
